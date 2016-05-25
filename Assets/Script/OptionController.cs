1f3f441cda7e1e80152fc7e23079b2307ea3b4f9
uichunter <pankettingpanda@gmail.com>
2016-05-25 15:40:42 -0500

    Finish credit scene and option scene.
    Import option controller and music controller.
    
    TODO: Add GameManager.

---
+++ Assets/Script/OptionController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class OptionController : MonoBehaviour {
+
+	public Slider volumeSlider;
+	public Slider difficultySlider;
+
+	float defultMusicVolume = 1f;
+	float defultDifficulty = 1f;
+	private MusicManager musicManager;
+	// Use this for initialization
+	void Start () {
+		musicManager = GameObject.FindObjectOfType<MusicManager>();
+		volumeSlider.value = PlayerPresfsManager.GetMasterVolume();
+		difficultySlider.value = PlayerPresfsManager.GetDifficulty();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		musicManager.SetMusicVolume(volumeSlider.value);
+	}
+
+	public void Save(){
+		PlayerPresfsManager.SetMasterVolume(volumeSlider.value);
+		PlayerPresfsManager.SetDifficulty(difficultySlider.value);
+	}
+
+	public void Defult ()
+	{
+		PlayerPresfsManager.SetMasterVolume(defultMusicVolume);
+		volumeSlider.value = defultMusicVolume;
+		PlayerPresfsManager.SetDifficulty(defultDifficulty);
+		difficultySlider.value = defultDifficulty;
+	}
+}
---
+++ Assets/Script/PlayerPrefsManager.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class PlayerPresfsManager : MonoBehaviour {
+	const string MASTER_VOLUME_KEY = "master_volume";
+	const string DIFFICULTY_KEY = "difficulty";
+	const string LEVEL_UNLOCKED_KEY = "level_unlocked_";
+
+	public static void SetMasterVolume (float volume)
+	{
+		if (volume <= 1f && volume >= 0f) {
+			PlayerPrefs.SetFloat (MASTER_VOLUME_KEY, volume);
+		} else {
+			Debug.LogError("Master volume is out of range.");
+		}
+	}
+
+	public static float GetMasterVolume ()
+	{
+		return PlayerPrefs.GetFloat(MASTER_VOLUME_KEY);
+	}
+
+	public static void SetLevelUnlocked (int level)
+	{
+		if (level<= SceneManager.sceneCountInBuildSettings -1) {
+			PlayerPrefs.SetInt (LEVEL_UNLOCKED_KEY+level.ToString(), 1);// 1 means true;
+		} else {
+			Debug.LogError("Level "+level+" is not in build setting list.");
+		}
+	}
+
+	public static bool GetLevelUnlocked (int level)
+	{
+		if (level <= SceneManager.sceneCountInBuildSettings - 1) {
+			if (PlayerPrefs.GetInt (LEVEL_UNLOCKED_KEY + level.ToString()) == 1) {
+				return true;
+			} else {
+				Debug.LogError ("Level " + level + " is locked.");
+				return false;
+			}
+		} else {
+			Debug.LogError("Level "+level+" is not in build setting list.");
+			return false;
+		}
+	}
+
+	public static void SetDifficulty (float volume)
+	{
+		if (volume <= 3f && volume >= 1f) {
+			PlayerPrefs.SetFloat (DIFFICULTY_KEY, volume);
+		} else {
+			Debug.LogError("Difficulty volume is out of range.");
+		}
+	}
+
+	public static float GetDifficulty ()
+	{
+		return PlayerPrefs.GetFloat(DIFFICULTY_KEY);
+	}
+
+}
